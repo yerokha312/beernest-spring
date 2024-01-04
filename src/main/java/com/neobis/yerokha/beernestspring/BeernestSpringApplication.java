982f0a2ed44f4a9ad8d9407ac967225716fff607
yerbolat <erbolatt@live.com>
2024-01-04 16:33:09 +0600

    created spring project for beernest

---
+++ src/main/java/com/neobis/yerokha/beernestspring/BeernestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.neobis.yerokha.beernestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeernestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeernestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neobis/yerokha/beernestspring/BeernestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.yerokha.beernestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeernestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
