2e3081e1ad5134af8692022d7f9b2e0291dd00be
yerbolat <erbolatt@live.com>
2024-01-11 18:42:54 +0600

    added SecurityConfiguration

--- src/main/java/com/neobis/yerokha/beernestspring/config/WebSecurityConfig.java
+++ src/main/java/com/neobis/yerokha/beernestspring/config/WebSecurityConfig.java
@@ -2,12 +2,16 @@ package com.neobis.yerokha.beernestspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
